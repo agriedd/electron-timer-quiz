90c64e75c7c1713a8df35fe4244c15e1bb532b54
Agri Edd. Oematan <dnr739528@gmail.com>
2022-08-13 22:59:14 +0800

    fuck nano staged

--- .nano-staged.mjs
+++ .nano-staged.mjs
@@ -1,28 +1,31 @@
 import {resolve, sep} from 'path';
 
 export default {
-  '*.{js,mjs,cjs,ts,mts,cts,vue}': (files) => {
-    console.log(files);
+  '*.{md}': (files) => {
     return 'eslint --cache --fix'
   },
+  // '*.{js,mjs,cjs,ts,mts,cts,vue}': (files) => {
+  //   console.log(files);
+  //   return 'eslint --cache --fix'
+  // },
 
   /**
    * Run typechecking if any type-sensitive files or project dependencies was changed
    * @param {string[]} filenames
    * @return {string[]}
    */
-  '{package-lock.json,packages/**/{*.ts,*.vue,tsconfig.json}}': ({filenames}) => {
+  // '{package-lock.json,packages/**/{*.ts,*.vue,tsconfig.json}}': ({filenames}) => {
 
-    // if dependencies was changed run type checking for all packages
-    if (filenames.some(f => f.endsWith('package-lock.json'))) {
-      return ['npm run typecheck --if-present'];
-    }
+  //   // if dependencies was changed run type checking for all packages
+  //   if (filenames.some(f => f.endsWith('package-lock.json'))) {
+  //     return ['npm run typecheck --if-present'];
+  //   }
 
-    // else run type checking for staged packages
-    const fileNameToPackageName = filename =>
-      filename.replace(resolve(process.cwd(), 'packages') + sep, '').split(sep)[0];
-    return [...new Set(filenames.map(fileNameToPackageName))].map(
-      p => `npm run typecheck:${p} --if-present`,
-    );
-  },
+  //   // else run type checking for staged packages
+  //   const fileNameToPackageName = filename =>
+  //     filename.replace(resolve(process.cwd(), 'packages') + sep, '').split(sep)[0];
+  //   return [...new Set(filenames.map(fileNameToPackageName))].map(
+  //     p => `npm run typecheck:${p} --if-present`,
+  //   );
+  // },
 };
